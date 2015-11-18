6f0460c20fa017dc10b34903541723f3a0c11882
Vicki Grunewald <vicki.grunewald@gmail.com>
2015-11-17 17:17:50 -0800

    Use jQuery to change paragraph style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
     var paragraphs = $( 'p' );
+    paragraphs.css({
+        "color": "red",
+        "background-color": "yellow",
+        "text-transform": "uppercase"
+    });
 });
